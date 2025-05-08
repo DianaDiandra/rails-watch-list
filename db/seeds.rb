89ab52cd9480a5e17524b27c19c5a324b82fc166
Diana Culita <diana.culita100@gmail.com>
2025-05-08 16:00:52 +0100

    ROUTES-CONTROLLER-VIEWS

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+puts "Fetching movies from TMDB..."
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+response = URI.open(url).read
+movies = JSON.parse(response)['results']
+
+puts "Creating movies..."
+movies.each do |movie|
+  poster_path = movie['poster_path']
+  image_url = "https://image.tmdb.org/t/p/w500#{poster_path}"
+
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: image_url,
+    rating: movie['vote_average']
+  )
+end
+
+puts "Created #{Movie.count} movies!"
+
+puts "Creating lists..."
+List.create!(name: "Action Favorites")
+List.create!(name: "Rom-Coms")
+List.create!(name: "Watch Later")
+puts "Created #{List.count} lists!"
